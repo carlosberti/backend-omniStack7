8b04837e83d114dcc069486175680f393e3defc0
Carlos Berti <carlosberti182@gmail.com>
2020-07-13 18:58:56 -0300

    Update listen port

--- src/index.ts
+++ src/index.ts
@@ -25,4 +25,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resiz
 
 app.use(routes);
 
-server.listen(3333, () => console.log('Listening on localhost:3333'));
+server.listen(process.env.PORT || 3333, () => console.log('Listening on localhost:3333'));
